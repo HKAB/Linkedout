b7a8775a8320a8965baef39fd015f18f8a4f7f17
Truong <khoahockithuatmc@gmail.com>
2020-07-17 23:46:52 +0700

    first

---
+++ frontend/src/app/Index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Nav } from '@/components';
+
+function App() {
+  return (
+    <div className="app-container">
+    <Nav />
+    This is app
+    </div>
+  );
+}
+
+export { App };
---
+++ frontend/src/home/Index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Home() {
+    
+    return (
+    	<div>This home is not a house</div>
+    );
+}
+
+export default Home;
