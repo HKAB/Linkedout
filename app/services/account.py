c7473fec472f3ef0fd608fa51869f9d9042a749b
wildcat <letranhaitung@gmail.com>
2020-07-20 00:56:55 +0700

    add checks for username, password, email

--- app/services/account.py
+++ app/services/account.py
@@ -1,13 +1,14 @@
 from hashlib import sha256
 import datetime
 import jwt
+import re
 
 from django.db import transaction
 
 from backend.settings import SECRET_KEY
 from app.models.account import Account
-from app.models.email import Email
 from app.services.email import create_email
+from app.exceptions import InvalidInputFormat
 
 PASSWORD_SALT = 'SALT'
 
@@ -16,6 +17,8 @@ def create_account(*, username: str, password: str, email: str) -> Account:
     """
     Create user account if not exist. Return Account object on success, None on failure.
     """
+    username_format_check(username)
+    password_format_check(password)
     account = Account.objects.filter(username=username).first()
     if account:
         return None
@@ -47,3 +50,23 @@ def generate_access_token(account: Account) -> str:
     }
     access_token = jwt.encode(access_token_payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
     return access_token
+
+def username_format_check(username: str, raise_exception=True) -> bool:
+    if len(username) > 36 or len(username) < 6:
+        if raise_exception:
+            raise InvalidInputFormat("Username must be 6 to 36 characters long.")
+        return False
+    match = re.fullmatch(r'[A-Za-z0-9.]+', username)
+    if not match:
+        if raise_exception:
+            raise InvalidInputFormat("Username must be alphanumeric")
+        return False
+    return True
+
+def password_format_check(password: str, raise_exception=True) -> bool:
+    if len(password) > 36 or len(password) < 8:
+        if raise_exception:
+            raise InvalidInputFormat("Password must be 8 to 36 characters long.")
+        return False
+    return True
+    
