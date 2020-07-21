465e0b4c2369920e102336605bf8bd81eb1dfed3
wildcat <letranhaitung@gmail.com>
2020-07-22 01:19:04 +0700

    oops forgot about account type :)

--- app/models/account.py
+++ app/models/account.py
@@ -3,3 +3,4 @@ from django.db import models
 class Account(models.Model):
     username = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
+    account_type = models.CharField(max_length=10)
