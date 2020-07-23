300f32a799b48c90eb89eff7679b5df9107d5dee
wildcat <letranhaitung@gmail.com>
2020-07-24 02:36:06 +0700

    school description can be null

--- app/models/school.py
+++ app/models/school.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class School(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField(max_length=512)
+    description = models.TextField(max_length=512, null=True)
     
