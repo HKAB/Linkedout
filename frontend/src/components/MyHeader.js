fcb7c345bb7330d1b6fe214049bc666b940ae090
Truong <khoahockithuatmc@gmail.com>
2020-07-25 02:03:28 +0700

    fix header a little

--- frontend/src/components/MyHeader.js
+++ frontend/src/components/MyHeader.js
@@ -12,22 +12,30 @@ const {SubMenu}=  Menu
 
 const { Header, Content } = Layout;
 
+const menu = (
+    <Menu>
+        <Menu.Item key='4' icon={<UserOutlined/>}>Account</Menu.Item>
+        <Menu.Item key='5' icon={<SettingOutlined/>}>Settings</Menu.Item>
+        <Menu.Item key='6' icon={<LogoutOutlined/>}>Sign out</Menu.Item>
+    </Menu>
+    )
+
 class MyHeader extends Component {
   
-  state = {
-    collapsed: false,
-    
-  };
-  toggle = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  };
+    state = {
+        collapsed: false,
+    };
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
+
 	render(){
 		return(
     <Header className="site-layout-background">
       <div className="logo" style={{fontWeight:"bold"}}>
-          <img src={logo} style={{width:32, height:32}}></img>
+          <img src={logo} style={{width:32, height:32, marginRight: 10}}></img>
           SUNSHINE
       </div>
        
@@ -38,18 +46,34 @@ class MyHeader extends Component {
            onClick: this.toggle,
         })}
       </Button>
-      
-      <Menu  theme="white" mode="horizontal" defaultSelectedKeys={['1']} style={{position:'absolute',right:24}}>
+		<span class="left-menu">
+		<Button type="text" style={{height: 64}}><QuestionCircleOutlined/></Button>
+			<Button type="text" style={{height: 64}}><BellOutlined/></Button>
+			<Dropdown overlay={menu} placement="bottomCenter" icon={<UserOutlined />}>
+				<Button type="text" style={{height: 64}}>
+					<Avatar className="avatar-picture" src={avata}/>
+					<span class="username"> Vu Trang Linh</span>
+				</Button>
+			</Dropdown>
+		</span>
+
+      {/* <Menu theme="white" mode="horizontal" defaultSelectedKeys={['1']} style={{position:'absolute',right:24}}>
         <Menu.Item key="1" icon={<QuestionCircleOutlined/>}></Menu.Item>
         <Menu.Item key="2" icon={<BellOutlined/>} ></Menu.Item>
-    
-         <Avatar src={avata} style={{margin:16}}></Avatar>
-         <SubMenu  key="3" title="VuTrangLinh" >
-           <Menu.Item key='4' icon={<UserOutlined/>}>Account</Menu.Item>
-          <Menu.Item key='5' icon={<SettingOutlined/>}>Settings</Menu.Item>
-          <Menu.Item key='6' icon={<LogoutOutlined/>}>Sign out</Menu.Item>
-        </SubMenu>
-      </Menu>
+
+        <Dropdown overlay={menu} placement="bottomLeft">
+          <Button>bottomLeft</Button>
+        </Dropdown>
+
+        <Menu.Item key="3">
+            <Avatar src={avata} /><span> Vu Trang Linh</span>
+                    <Dropdown overlay={menu} placement="bottomLeft">
+                    <span>AAAA</span>
+        </Dropdown>
+        </Menu.Item>
+      </Menu> */}
+
+      
     </Header>
     
 			);
