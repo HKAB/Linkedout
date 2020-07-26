1c3446b05ff762d044257acc4c9f008781da01ee
ultoxtung <tunglexth@gmail.com>
2020-07-26 15:02:14 +0700

    tag

--- app/views/tag.py
+++ app/views/tag.py
@@ -6,23 +6,106 @@ from rest_framework.response import Response
 from rest_framework import status, serializers
 from drf_yasg.utils import swagger_auto_schema
 
-from app.services.tag import get_skill_tag, get_title_tag
+from app.services.tag import get_skill_tag, get_title_tag, get_school_tag, get_company_tag
 
 class SkillTagView(APIView):
     class InputSerializer(serializers.Serializer):
-        id = serializers.CharField(required=True)
+        query = serializers.CharField(required=True)
 
         class Meta:
-            ref_name = 'SkillTag'
+            ref_name = 'SkillTagIn'
             fields = ['query']
 
     class OutputSerializer(serializers.Serializer):
-        pass
+        tag = serializers.ListField()
 
-    @swagger_auto_schema(query_serializer=InputSerializer, responses={200: Response()})
+        class Meta:
+            ref_name = 'SkillTagOut'
+            fields = ['tag']
+
+    permission_classes = [AllowAny]
+
+    @swagger_auto_schema(query_serializer=InputSerializer, responses={200: OutputSerializer})
     @method_decorator(ensure_csrf_cookie)
     def get(self, request):
         serializers = self.InputSerializer(data=request.query_params)
         serializers.is_valid(raise_exception=True)
         result = get_skill_tag(**serializers.validated_data)
-        return Response(result, status=status.HTTP_200_OK)
+        return Response(self.OutputSerializer(result).data, status=status.HTTP_200_OK)
+
+class TitleTagView(APIView):
+    class InputSerializer(serializers.Serializer):
+        query = serializers.CharField(required=True)
+
+        class Meta:
+            ref_name = 'TitleTagIn'
+            fields = ['query']
+
+    class OutputSerializer(serializers.Serializer):
+        tag = serializers.ListField()
+
+        class Meta:
+            ref_name = 'TitleTagOut'
+            fields = ['tag']
+
+    permission_classes = [AllowAny]
+
+    @swagger_auto_schema(query_serializer=InputSerializer, responses={200: OutputSerializer})
+    @method_decorator(ensure_csrf_cookie)
+    def get(self, request):
+        serializers = self.InputSerializer(data=request.query_params)
+        serializers.is_valid(raise_exception=True)
+        result = get_title_tag(**serializers.validated_data)
+        return Response(self.OutputSerializer(result).data, status=status.HTTP_200_OK)
+
+class SchoolTagView(APIView):
+    class InputSerializer(serializers.Serializer):
+        query = serializers.CharField(required=True)
+
+        class Meta:
+            ref_name = 'SchoolTagIn'
+            fields = ['query']
+
+    class OutputSerializer(serializers.Serializer):
+        name = serializers.CharField()
+        logo = serializers.ImageField()
+
+        class Meta:
+            ref_name = 'SchoolTagOut'
+            fields = ['name', 'logo']
+
+    permission_classes = [AllowAny]
+
+    @swagger_auto_schema(query_serializer=InputSerializer, responses={200: OutputSerializer})
+    @method_decorator(ensure_csrf_cookie)
+    def get(self, request):
+        serializers = self.InputSerializer(data=request.query_params)
+        serializers.is_valid(raise_exception=True)
+        result = get_school_tag(**serializers.validated_data)
+        return Response(self.OutputSerializer(result, many=True).data, status=status.HTTP_200_OK)
+
+class CompanyTagView(APIView):
+    class InputSerializer(serializers.Serializer):
+        query = serializers.CharField(required=True)
+
+        class Meta:
+            ref_name = 'CompanyTagIn'
+            fields = ['query']
+
+    class OutputSerializer(serializers.Serializer):
+        name = serializers.CharField()
+        logo = serializers.ImageField()
+
+        class Meta:
+            ref_name = 'CompanyTagOut'
+            fields = ['name', 'logo']
+
+    permission_classes = [AllowAny]
+
+    @swagger_auto_schema(query_serializer=InputSerializer, responses={200: OutputSerializer})
+    @method_decorator(ensure_csrf_cookie)
+    def get(self, request):
+        serializers = self.InputSerializer(data=request.query_params)
+        serializers.is_valid(raise_exception=True)
+        result = get_company_tag(**serializers.validated_data)
+        return Response(self.OutputSerializer(result, many=True).data, status=status.HTTP_200_OK)
