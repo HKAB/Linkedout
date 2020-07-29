f3a8e06715ceca25c56eac8dc0bfea52a684201f
NXT <xuantruongd9@gmail.com>
2020-07-29 23:12:24 +0700

    add form to profile change

--- frontend/src/profile/ProfileChange.js
+++ frontend/src/profile/ProfileChange.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Component } from 'react';
-import {EditOutlined, DeleteOutlined} from '@ant-design/icons';
+import {EditOutlined, DeleteOutlined, PlusOutlined, MinusCircleOutlined} from '@ant-design/icons';
 
-import { Skeleton, Switch, Card, Button, Timeline } from 'antd';
+import { Skeleton, Switch, Card, Button, Timeline, Form,Input, Space, DatePicker} from 'antd';
 import Meta from 'antd/lib/card/Meta';
 
 import { List, Avatar } from 'antd';
 import Item from 'antd/lib/list/Item';
-
+//import Form from 'antd/lib/form/Form';
+import { ProfileChangeServices } from "@/services"
+const { RangePicker } = DatePicker;
 const data_experiences = [
+    
     {
         avatar: 'https://image.flaticon.com/icons/svg/508/508123.svg',
         companyName: 'Facebook',
@@ -69,16 +72,69 @@ const data_experiences = [
         major: "IT"
     },
   ];
+  const skill_data = [
+    {
+      avatar:'https://dwglogo.com/wp-content/uploads/2017/09/c_logo.png',
+      title_href:'https://ant.design',
+      title: 'C++',
+    },
+    {
+      avatar:'https://image.flaticon.com/icons/svg/919/919851.svg',
+      title_href:'https://ant.design',
+      title: 'React',
+    },
+    {
+      avatar:'https://image.flaticon.com/icons/svg/1822/1822899.svg',
+      title_href:'https://ant.design',
+      title: 'Python',
+    },
+    {
+      avatar:'https://image.flaticon.com/icons/svg/1387/1387539.svg',
+      title_href:'https://ant.design',
+      title: 'Java',
+    },
+    {
+      avatar:'https://image.flaticon.com/icons/svg/919/919830.svg',
+      title_href:'https://ant.design',
+      title: 'PHP',
+    },
+    ];
 
   const timeline_element = [];
   data_education.forEach(item => {
-      timeline_element.push(<Timeline.Item label={item.start_date + " - " + item.end_date}><div><b>{item.school_name}</b></div><div>{"Degree: " + item.degree}</div><div>{"Major: " + item.major}</div><DeleteOutlined style={{color: "red"}}/></Timeline.Item>);
+      timeline_element.push(<Timeline.Item key = {item.school_name} label={item.start_date + " - " + item.end_date}><div><b>{item.school_name}</b></div><div>{"Degree: " + item.degree}</div><div>{"Major: " + item.major}</div>
+      <DeleteOutlined style={{color: "red"}} 
+      onClick={() =>  {
+        for (let id = 0; id < timeline_element.length;id++) {
+            if (timeline_element[id].key == item.school_name) {
+                console.log(item.school_name);
+                timeline_element.splice(id,1);
+                break;
+            }
+        }
+        //console.log(timeline_element.length);
+    } }/></Timeline.Item>);
   });
+
+  const addExpFinish = values => {
+		console.log(values);
+		ProfileChangeServices.addexp(values.companyname, values.description, values.title, values.startdate, values.enddate).then()
+		.then(() => {
+			alert("Experiences added");
+		//	history.go();
+		})
+		.catch(error => {
+			alert(error);
+		});
+	}
+
 
 class ProfileChange extends Component {
     state = {  
-        tabPosition: 'left'
-    }
+        tabPosition: 'left',
+    };
+    
+    
     
     render() { 
         return ( 
@@ -107,12 +163,89 @@ class ProfileChange extends Component {
                                 description={item.time}
                                 style={{marginRight: 48}}
                             />
-                            <Button type="dashed" shape="circle" icon={<EditOutlined/>} />
+                          <Button type="dashed" shape="circle" icon={<EditOutlined/>}/>
+                            
                         </List.Item>
                     )}>
+                    <Form name="add-exp" autoComplete="off" onFinish={addExpFinish}>
+              <Form.List name="exp-info">
+                {(fields, { add, remove }) => {
+                  return (
+                    <div>
+                     {fields.map(field => (
+                       <Space key={field.key} style={{ display: 'flex', marginBottom: 8, marginTop:8 }} align="start">
+                          <Form.Item //name = "companyname"
+                            {...field}
+                            name={[field.name, 'companyname']}
+                            fieldKey={[field.fieldKey, 'companyname']}
+                            rules={[{ required: true, message: 'Missing company name' }]}
+                          > 
+                            
+                            <Input placeholder="Company Name" />
+                          </Form.Item>
+                      
+                          <Form.Item //name = "description"
+                            {...field}
+                            name={[field.name, 'description']}
+                            fieldKey={[field.fieldKey, 'description']}
+                            rules={[{ required: true, message: 'Missing Description' }]}
+                          >
+                            <Input placeholder="Description" />
+                          </Form.Item>
+
+                          <Form.Item //name = "title"
+                            {...field}
+                            name={[field.name, 'title']}
+                            fieldKey={[field.fieldKey, 'title']}
+                            rules={[{ required: true, message: 'Missing title' }]}
+                          >
+                            <Input placeholder="Title" />
+                          </Form.Item>
+
+                          <Form.Item //name = "startdate"
+                            {...field}
+                            name={[field.name, 'startdate']}
+                            fieldKey={[field.fieldKey, 'startdate']}
+                            rules={[{ required: true, message: 'Missing start date' }]}
+                          >
+                            <DatePicker placeholder = "Start date"/>
+                          </Form.Item>
+
+                          <Form.Item //name = "enddate"
+                            {...field}
+                            name={[field.name, 'enddate']}
+                            fieldKey={[field.fieldKey, 'enddate']}
+                            rules={[{ required: false }]}
+                          >
+                            <DatePicker placeholder = "End date"/>
+                          </Form.Item>
+
+                          <MinusCircleOutlined
+                            onClick={() => {
+                              remove(field.name);
+                            }}
+                          />
+                        </Space>
+                     ))}
+
+                     <Form.Item>
+                        <Button type="dashed" onClick={() => {add()}} block>
+                          <PlusOutlined /> Add Experience
+                        </Button>
+                      </Form.Item>
+                    </div>
+                  );
+                }}
+              </Form.List>
+
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+             </Form.Item>
+            </Form>
                 </List>
             </Card>
-
             <Card style={{marginTop: 24}}>
                 <Meta title="Education" ></Meta>
                 <Timeline mode="left" style={{marginTop: 24}}>
@@ -122,6 +255,24 @@ class ProfileChange extends Component {
                     <Timeline.Item label="2015-09-01 09:12:11">Network problems being solved</Timeline.Item> */}
                 </Timeline>
             </Card>
+            <Card style = {{marginTop: 24}}>
+			        <Meta  title="Skill"></Meta>
+              <List style = {{marginTop:24}} grid = {{column:2}}
+                dataSource = {skill_data}
+                renderItem = {item =>(
+                  <List.Item>
+                    <List.Item.Meta
+                      avatar={<Avatar src = {item.avatar}/>}
+                      title ={<a href={item.title_href}>{item.title}</a>}
+                    />
+                  </List.Item>
+                )}
+              />
+              <Button type="dashed" shape = "circle">
+                    <PlusOutlined />
+                </Button> 
+            </Card>
+            
             </>
          );
     }
