53f23b9fa331ddad57cda7f22b2c50a2f71176e6
NXT <xuantruongd9@gmail.com>
2020-07-31 15:49:54 +0700

    add createStudent

--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from '@/home'
 import { Profile } from '@/profile'
 import { Login } from '@/account'
+import { createStudent } from '@/account';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
 	    	<Route exact path='/' component={ Home }/>
 	    	<Route exact path='/login' component={ Login }/>
 	    	<Route exact path='/profile' component={ Profile }/>
+			<Route exact path='/createStudent' component = {createStudent}/>
 	    </Switch>
     </BrowserRouter>
     </div>
