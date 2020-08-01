aaac0fc0e24cc879df1c88b1b1082b793a4e494f
ultoxtung <tunglexth@gmail.com>
2020-08-01 16:05:23 +0700

    store company picture

--- app/models/company.py
+++ app/models/company.py
@@ -5,6 +5,11 @@ from .student import Student
 from .specialty import Specialty
 
 
+def store_picture(instance, filename: str) -> str:
+    extension = filename.split('.')[-1]
+    return "profile/company_" + "{}.{}".format(instance.account.username, extension)
+
+
 class Company(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
