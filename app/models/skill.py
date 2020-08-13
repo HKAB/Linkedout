429f224ab6600174e1bd0f8c16e5be5f4055c376
ultoxtung <tunglexth@gmail.com>
2020-08-13 16:43:35 +0700

    Add a dummy function so that makemigrations thing don't throw error (will delete later)

--- app/models/skill.py
+++ app/models/skill.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+def store_picture ():
+    # this is a dummy function, only here so that that makemigrations thing can work
+    pass
+
 class Skill(models.Model):
     name = models.CharField(max_length=64)
 
