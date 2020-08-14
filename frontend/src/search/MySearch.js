4883326a6392e049ddacc866d40ce75a76efb6c1
VuThiDiu <56347983+VuThiDiu@users.noreply.github.com>
2020-08-14 08:46:59 +0700

    ...

--- frontend/src/search/MySearch.js
+++ frontend/src/search/MySearch.js
@@ -1,49 +1,27 @@
-import React, {useState, useEffect} from 'react';
-import { Card, Row , Col, Layout, Typography, Input, Tag, AutoComplete, Avatar} from 'antd';
-import Meta from 'antd/lib/card/Meta';
-import{EditableTagGroup} from 'components'
-import { List } from 'antd/lib/form/Form';
-import create from '@ant-design/icons/lib/components/IconFont';
-const { Title } = Typography;
-const {Search} = Input
+import React from 'react';
+import { Input, Layout, Row, Col, Select } from 'antd'
 
+const { Search } = Input;
+const { Option } = Select;
 
 function MySearch() {
-   
-    const onChangeComplete =  (values) =>{
-       
-    }
-    
     return (
-        <Row>
-        <Col></Col>
-        <Col span={24}>
-          <Layout style={{ padding: "0 24px 24px" }}>
+        <Layout>
             <Row>
-              <Col span={4} style={{ paddingLeft: "5vw", marginTop: "20vh" }}>
-                
-              </Col>
-              <Col span={16}>
-                  <Card className="card-info" style={{ marginTop: 24 }}>
-                  <Meta title={<Title level={3}>Search</Title>}></Meta>
-                      <Col span={12} offset={5}>
-                                <Input.Search
-                                    placeholder="Search St"
-                                    onSearch={onChangeComplete}
-                                    style={{ marginTop: 15, marginLeft: 30, }}
-                                    />   
-                    </Col>
-                   
-               
-                  </Card>
-                
-              </Col>
-              <Col span={4}></Col>
+                <Col span={8}></Col>
+                <Col span={8}>
+                <Input.Group compact>
+                    <Select defaultValue="student" style={{ width: '30%' }}>
+                        <Option value="company">Company</Option>
+                        <Option value="student">Student</Option>
+                        <Option value="skill">Skill</Option>
+                    </Select>
+                    <Input placeholder="Bạn muốn search gì?" style={{ width: '70%' }}></Input>
+                </Input.Group>
+                </Col>
+                <Col span={8}></Col>
             </Row>
-          </Layout>
-        </Col>
-        <Col></Col>
-      </Row>
+        </Layout>
     )
 }
 
