24b55d349b1221b101080f58529e30b7a876ffb6
ultoxtung <tunglexth@gmail.com>
2020-08-16 20:52:31 +0700

    [pls pip install -r requirement.txt before processsing] Early stage admin site. Gotta do FHC now.

--- backend/urls.py
+++ backend/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls.static import static
-from django.contrib import admin
+from baton.autodiscover import admin
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -42,6 +42,7 @@ urlpatterns = [
                                          cache_timeout=0), name='schema-redoc'),
     url('', include('django_prometheus.urls')),
     path('admin/', admin.site.urls),
+    path('baton/', include('baton.urls')),
     path('api/', include('app.urls')),
 ]
 
